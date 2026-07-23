4f7e1378a3b832de30c1451374464eae6236fee9
Anica <3c.innertherapy@gmail.com>
2026-07-24 00:38:02 +0100

    Create manifest.js

---
+++ key-notes/manifest.js
@@ -0,0 +1,22 @@
+{
+  "name": "KEY Notes",
+  "short_name": "KEY Notes",
+  "description": "Your private, encrypted archive. Contacts, links, notes, and keys.",
+  "start_url": "./index.html",
+  "display": "standalone",
+  "background_color": "#080b1a",
+  "theme_color": "#080b1a",
+  "orientation": "portrait-primary",
+  "icons": [
+    {
+      "src": "../assets/favicon.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "../assets/favicon.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
